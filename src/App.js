06835df8c02eec3623cae9b890bf18f561a28979
MaB@2210 <mannbhatt.00@gmail.com>
2022-11-05 13:24:12 -0400

    Test4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
-import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './About.css'
 const About = () => {
   return (
     <div>
         <div-card>
-            <h1> Are you Bored </h1>
+            <h1> Are you Bored?</h1>
             <h2> Have a look at what's happening around</h2>
         </div-card>
 
@@ -13,9 +13,12 @@ const About = () => {
             <h4>
                 If you're bored & Don't konw what to do
                 Have a look at the events happening around you
-                
             </h4>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
         </div-about>
+       
         
     </div>
   )
