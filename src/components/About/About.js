b5f86a25481494f2630b648e1affd9398e49be85
MaB@2210 <mannbhatt.00@gmail.com>
2022-11-06 10:49:48 -0500

    Final FrontEnd1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,8 +19,15 @@ const About = () => {
                     </div>  
 
                     <div className='home_About'>
-                        <h1>What is Project</h1>
-                        <p className="About_txt">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <h1>What is EventsHub</h1>
+                        <p className="About_txt">EventsHub is a project dedicated to streamlining the process of forming relationships and gatherings. Eventhub will allow users to post their gatherings and events on our website, allowing for other users to find it and join. This makes it much easier to put together a group of people for a beach volleyball game, a potluck, a concert, and more!
+
+                        <b>Create Events for Others to Join:</b>
+                        Have you ever started a game of volleyball or frisbee and found yourself with a shortage of people? Post your game on EventsHub, and people will be able to find your game on our website!
+
+                        <b>Join Events Near You:</b>
+                        Bored and looking for a friendly ball game nearby? EventsHub can give you a list of all open gatherings near you! Look for ball games, special events, barbecues, and more! 
+.</p>
                     </div>  
             </div>
         </div>
